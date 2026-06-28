57cd54cebc8c6906292387d77639e79c9c4fb037
Adithyanair098 <adithyanair098@gmail.com>
2026-06-28 20:11:00 +0530

    security: move Gemini API key to environment variables

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'app_theme.dart';
 import 'widgets/main_scaffold.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
 
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
